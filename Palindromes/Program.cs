88ea0902ef46e081b24675e356da958d02486b2e
Mike Falzerano <mfalzera@yahoo.com>
2018-08-11 23:09:38 -0400

    Initial commit after completing the C# Palindrome example on DotNetPerls.

---
+++ Palindromes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Palindromes
+{
+    class Program
+    {
+        /// <summary>
+        /// Determines if the given value is a palindrome.
+        /// </summary>
+        /// <returns><c>true</c>, if value is a palindrome, <c>false</c> otherwise.</returns>
+        /// <param name="value">String to evaluate</param>
+        public static bool IsPalindrome(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+
+            while (true)
+            {
+                if (min > max)
+                    return true;
+
+                char a = value[min];
+                char b = value[max];
+
+                if (char.ToLower(a) != char.ToLower(b))
+                    return false;
+
+                min++;
+                max--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] array =
+            {
+                "civic",
+                "racecar",
+                "mike",
+                "solos"
+            };
+
+            // loop through the array and determine which words are palindromes
+            foreach (var value in array)
+            {
+                Console.WriteLine("{0} = {1}", value, IsPalindrome(value));
+            }
+            Console.WriteLine("<<Processing Complete>>");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Palindromes
+Palindromes in C# whiteboard example from DotNetPerls.
+
+[DotNetPerls tutorial](https://www.dotnetperls.com/palindrome)
